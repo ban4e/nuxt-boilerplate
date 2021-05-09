84e2ea78c0c9fe5dc9f7dcb5728f413fa00ad8d2
ban4e <kocomst@gmail.com>
2021-05-09 15:34:07 +0700

    Dockerize nuxt. Set basic settings

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="body__fallback">
+        <div class="body__wrapper">
+            <div class="body__header"></div>
+            <div class="body__container">
+                <Nuxt />
+            </div>
+            <div class="body__footer"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            bodyAttrs: {
+                class: 'body'
+            }
+        };
+    }
+};
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="grid-container mt-32">
+        <div class="grid-list">
+            <div class="grid-item grid-item_4x">Сетка</div>
+            <div class="grid-item grid-item_4x">Сетка</div>
+            <div class="grid-item grid-item_4x">Сетка</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
